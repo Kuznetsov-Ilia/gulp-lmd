73155e24f96ff85439e791c3f84c1993542956b4
Kuznetsov Ilia <i.kuznecov@corp.mail.ru>
2014-10-03 18:56:54 +0400

    clear comments

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const PLUGIN_NAME = 'gulp-lmd';
 var through = require('through2');
 var gutil = require('gulp-util');
 var log = gutil.log;
-var PluginError = gutil.PluginError;
 var LMD = require('lmd');
 
 module.exports = function (path) {
@@ -15,23 +13,10 @@ module.exports = function (path) {
       lmd.log.pipe(process.stdout);
 
       file.contents = streamer;
-
       this.push(file);
       return callback();
-
-    } else if (file.isStream()) {
-      /*var lmd = new LMD(path);      
-      var streamer = through();
-      streamer.on('error', this.emit.bind(this, 'error'));
-      lmd.pipe(streamer);
-      lmd.log.pipe(process.stdout);
-
-      file.contents = file.contents.pipe(streamer);
-
-      this.push(file);
-      return callback();*/
     } else {
-      log('only streams, try `gulp.src("conf.lmd.js", { buffer: false })`');
+      log('only streams, try `gulp.src('')`');
       this.push(file);
       return callback();
     }
